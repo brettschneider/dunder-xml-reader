aa304ecd7c54c2aa0b56d90ef4d7f801e01394f1
Steve Brettschneider <steve@bluehousefamily.com>
2022-08-08 16:08:41 -0400

    Convert to pyproject.toml in prep for PyPi.

--- setup.py
+++ setup.py
@@ -2,14 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    name='dunder_xml_reader',
-    description='A Pythonic XML parser',
-    author='Steve Brettschneider',
-    author_email='steve@bluehousefamily.com',
-    license='GPLv3',
-    packages=['dunder_xml_reader'],
-    zip_safe=False,
-    install_requires=[],
-    include_package_data=True
-)
+setup()
