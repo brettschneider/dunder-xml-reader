9ed3600d4d0685aea6c4d8ea8ce069c6a17668c7
Steve Brettschneider <steve@bluehousefamily.com>
2022-08-08 15:11:39 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='dunder_xml_reader',
+    description='A Pythonic XML parser',
+    author='Steve Brettschneider',
+    author_email='steve@bluehousefamily.com',
+    license='GPLv3',
+    packages=['dunder_xml_reader'],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True
+)
---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+"""pytest fixtures shared among modules"""
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def sample_xml_text():
+    with open(Path(__file__).parents[0] / 'sample_cxml.xml') as infile:
+        return infile.read()
