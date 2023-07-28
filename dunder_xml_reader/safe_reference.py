281f7590f401e92a1423844eff612d38c7d215db
Steve Brettschneider <steve.brettschneider@ameritas.com>
2023-07-28 08:47:52 -0400

    Now methods can be optionally not wrapped in SafeReference instances.

--- dunder_xml_reader/safe_reference.py
+++ dunder_xml_reader/safe_reference.py
@@ -4,17 +4,19 @@ import sys
 
 class SafeReference:
 
-    def __init__(self, object, default):
+    def __init__(self, object, default, wrap_methods):
         self.object = object
         self.default = default
+        self.wrap_methods = wrap_methods
 
     def __getattr__(self, item):
+        # noinspection PyBroadException
         try:
-            return SafeReference(getattr(self.object, item), self.default)
+            return SafeReference(getattr(self.object, item), self.default, self.wrap_methods)
         except:
             if self.object is not self.default:
                 print(f"SafeReference: Dereference fault: {self.object.__class__.__name__}.{item}", file=sys.stderr)
-            return SafeReference(self.default, self.default)
+            return SafeReference(self.default, self.default, self.wrap_methods)
 
     class Iterator:
         def __init__(self, obj):
@@ -32,10 +34,11 @@ class SafeReference:
         return SafeReference.Iterator(self)
 
     def __getitem__(self, item):
+        # noinspection PyBroadException
         try:
-            return SafeReference(self.object.__getitem__(item), self.default)
+            return SafeReference(self.object.__getitem__(item), self.default, self.wrap_methods)
         except:
-            return SafeReference(self.default, self.default)
+            return SafeReference(self.default, self.default, self.wrap_methods)
 
     def __len__(self):
         return len(self.object)
@@ -50,7 +53,10 @@ class SafeReference:
         return other == self.object
 
     def __call__(self, *args, **kwargs):
-        try:
-            return SafeReference(self.object(*args, *kwargs), self.default)
-        except TypeError:
-            return SafeReference(self.default, self.default)
+        if self.wrap_methods:
+            if callable(self.object):
+                return SafeReference(self.object(*args, *kwargs), self.default, self.wrap_methods)
+            else:
+                return SafeReference(self.default, self.default, self.wrap_methods)
+        else:
+            return self.object(*args, *kwargs)
