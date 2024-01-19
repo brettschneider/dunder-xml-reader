fdca4367b3f82d2038b30d7c870771e812191223
Steve Brettschneider <steve@bluehousefamily.com>
2024-01-18 20:53:15 -0500

    Working on workflows.

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,7 @@ if [[ $POETRY_ACTIVE == "1" ]]; then
   export PYTHONDONTWRITEBYTECODE=1
   rm -fr ./test_results 2> /dev/null
   mkdir ./test_results 2> /dev/null
-  pytest --cov=. --junitxml=test_results/results.xml -v && coverage html -d ./test_results/html
+  pytest --cov=. --junitxml=test_results/results.xml -v && coverage html -d ./test_results/html && coverage xml -o ./test_results/coverage.xml
   rm -r .pytest_cache 2> /dev/null
   rm -r __pycache__ 2> /dev/null
   rm -r .coverage 2> /dev/null
